3e054acde8aa6d90245c553357785214a10a0fbe
naveenaasre <naveenaasrekiruthe@gmail.com>
2024-02-01 18:59:11 +0530

    u-boot

---
+++ include/config.h
@@ -0,0 +1,6 @@
+/* Automatically generated - do not edit */
+#define CFG_BOARDDIR board/ti/am335x
+#include <configs/am335x_evm.h>
+#include <asm/config.h>
+#include <linux/kconfig.h>
+#include <config_fallbacks.h>
